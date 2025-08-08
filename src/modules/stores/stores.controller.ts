dd8a9679f089d0ee4ed9959b4ea2a88db7c7088c
* <frederickfrimpong284@gmail.comgit config --global user.name 1fred-cpu>
2025-08-08 09:08:16 +0000

    added a findStore method in storesService

--- src/modules/stores/stores.controller.ts
+++ src/modules/stores/stores.controller.ts
@@ -1,43 +1,43 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  ValidationPipe,
-} from '@nestjs/common';
-import { StoresService } from './stores.service';
-import { CreateStoreDto } from './dto/create-store.dto';
-import { UpdateStoreDto } from './dto/update-store.dto';
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    ValidationPipe
+} from "@nestjs/common";
+import { StoresService } from "./stores.service";
+import { CreateStoreDto } from "./dto/create-store.dto";
+import { UpdateStoreDto } from "./dto/update-store.dto";
 
-@Controller('stores')
+@Controller("stores")
 export class StoresController {
-  constructor(private readonly storesService: StoresService) {}
+    constructor(private readonly storesService: StoresService) {}
 
-  @Post('create')
-  createStore(@Body(ValidationPipe) createStoreDto: CreateStoreDto) {
-    return this.storesService.createStore(createStoreDto);
-  }
+    @Post("create")
+    async createStore(@Body(ValidationPipe) createStoreDto: CreateStoreDto) {
+        return this.storesService.createStore(createStoreDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.storesService.findAll();
-  }
+    @Get(":store_id")
+    async findStore(@Param("store_id") store_id: string) {
+        return this.storesService.findStore(store_id);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.storesService.findOne(+id);
-  }
+    @Get(":id")
+    findOne(@Param("id") id: string) {
+        return this.storesService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStoreDto: UpdateStoreDto) {
-    return this.storesService.update(+id, updateStoreDto);
-  }
+    @Patch(":id")
+    update(@Param("id") id: string, @Body() updateStoreDto: UpdateStoreDto) {
+        return this.storesService.update(+id, updateStoreDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.storesService.remove(+id);
-  }
+    @Delete(":id")
+    remove(@Param("id") id: string) {
+        return this.storesService.remove(+id);
+    }
 }
--- src/modules/stores/stores.service.ts
+++ src/modules/stores/stores.service.ts
@@ -1,78 +1,170 @@
 import {
-  Injectable,
-  Inject,
-  InternalServerErrorException,
-  ConflictException,
-  Logger,
-} from '@nestjs/common';
-import { CreateStoreDto } from './dto/create-store.dto';
-import { UpdateStoreDto } from './dto/update-store.dto';
-
+    Injectable,
+    Inject,
+    InternalServerErrorException,
+    ConflictException,
+    BadRequestException,
+    NotFoundException,
+    Logger
+} from "@nestjs/common";
+import { CreateStoreDto } from "./dto/create-store.dto";
+import { UpdateStoreDto } from "./dto/update-store.dto";
+import { isValidUUID } from "../../../utils/id-validator";
 @Injectable()
 export class StoresService {
-  private readonly logger = new Logger(StoresService.name);
-  constructor(
-    @Inject('SUPABASE_CLIENT') private readonly supabase: any, // Inject Supabase client
-  ) {}
+    private readonly logger = new Logger(StoresService.name);
+    constructor(
+        @Inject("SUPABASE_CLIENT") private readonly supabase: any // Inject Supabase client
+    ) {}
 
-  // Method -- Post
-  // Access -- Public
-  // Function:  A function to create a new store
-  // Returns: A success message or throws an error if the store already exists
-  async createStore(createStoreDto: CreateStoreDto) {
-    try {
-      // Check if the store already exists for the owner
-      const { data, error } = await this.supabase
-        .from('Stores')
-        .select('*')
-        .eq('owner_id', createStoreDto.owner_id)
-        .maybeSingle();
+    // Method -- Post
+    // Access -- Public
+    // Function:  A function to create a new store
+    // Returns: A success message or throws an error if the store already exists
+    async createStore(createStoreDto: CreateStoreDto) {
+        try {
+            // Check if the store already exists for the owner
+            const { data, error } = await this.supabase
+                .from("Stores")
+                .select("*")
+                .eq("owner_id", createStoreDto.owner_id)
+                .maybeSingle();
 
-      // If an error occurs or data is found, throw an error
-      if (error) {
-        throw new Error(error.message);
-      }
+            // If an error occurs or data is found, throw an error
+            if (error) {
+                throw new Error(
+                    "An error occured while  checking store existence"
+                );
+            }
 
-      if (data) {
-        throw new ConflictException('Store already exists');
-      }
+            if (data) {
+                throw new ConflictException("Store already exists");
+            }
 
-      // Create the new store
-      const { error: createError } = await this.supabase
-        .from('Stores')
-        .insert([{ ...createStoreDto }]);
+            // Create the new store
+            const { error: createError } = await this.supabase
+                .from("Stores")
+                .insert([{ ...createStoreDto }]);
 
-      if (createError) {
-        throw new Error(createError.message);
-      }
+            if (createError) {
+                throw new Error("An error occured while creating store");
+            }
 
-      return { message: 'Store created successfully' };
-    } catch (error) {
-      this.logger.error('Error creating store', error.message);
-      if (error instanceof Error) {
-        throw new InternalServerErrorException(error.message);
-      } else if (error instanceof ConflictException) {
-        throw error; // Re-throw ConflictException
-      }
-      throw new InternalServerErrorException(
-        'An unexpected error occurred while creating the store',
-      );
+            return { message: "Store created successfully" };
+        } catch (error) {
+            // Logs error to the  console
+            this.logger.error("Error creating store: ", error.message);
+
+            if (error instanceof Error) {
+                throw new InternalServerErrorException(error.message);
+            } else if (error instanceof ConflictException) {
+                throw error; // Re-throw ConflictException
+            }
+            throw new InternalServerErrorException(
+                "An unexpected error occurred while creating the store"
+            );
+        }
     }
-  }
 
-  findAll() {
-    return `This action returns all stores`;
-  }
+    // Method -- Get
+    // Access -- Private
+    // Function:  A function to find store
+    // Returns: The store to the client if found or throws an error if not found
+    async findStore(store_id: string) {
+        try {
+            // Validate the format of store_id if it matches uuid format
+            const isStoreIdValid = isValidUUID(store_id);
+            if (!isStoreIdValid) {
+                throw new BadRequestException("Invalid format of store_id");
+            }
 
-  findOne(id: number) {
-    return `This action returns a #${id} store`;
-  }
+            // Find a store with store_id
+            const { data, error } = await this.supabase
+                .from("Stores")
+                .select("*")
+                .eq("store_id", store_id)
+                .maybeSingle();
 
-  update(id: number, updateStoreDto: UpdateStoreDto) {
-    return `This action updates a #${id} store`;
-  }
+            // If an error occurs while finding store throw an error
+            if (error) {
+                throw new Error("An error occured while  finding store");
+            }
+            if (!data) {
+                throw new NotFoundException("Store not found");
+            }
+            // Returns data to client
+            return data;
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new InternalServerErrorException(error.message);
+            } else if (error instanceof BadRequestException) {
+                throw error;
+            } else if (error instanceof NotFoundException) {
+                throw error;
+            }
+            // Logs error to the  console
+            this.logger.error("Error finding store: ", error.message);
+            throw new InternalServerErrorException(
+                "An unexpected error occurred while finding  store"
+            );
+        }
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} store`;
-  }
+    // Method -- Patch
+    // Access -- Private
+    // Function:  A function to update store
+    /* Returns: Update the store if found and return updated store or throwa an
+    error if not found */
+    
+    async updateStore(store_id: string) {
+        try {
+            // Validate the format of store_id if it matches uuid format
+            const isStoreIdValid = isValidUUID(store_id);
+            if (!isStoreIdValid) {
+                throw new BadRequestException("Invalid format of store_id");
+            }
+
+            // Find a store with store_id
+            const { data, error } = await this.supabase
+                .from("Stores")
+                .select("*")
+                .eq("store_id", store_id)
+                .maybeSingle();
+
+            // If an error occurs while finding store throw an error
+            if (error) {
+                throw new Error("An error occured while  finding store");
+            }
+            if (!data) {
+                throw new NotFoundException("Store not found");
+            }
+            // Returns data to client
+            return data;
+        } catch (error) {
+            if (error instanceof Error) {
+                throw new InternalServerErrorException(error.message);
+            } else if (error instanceof BadRequestException) {
+                throw error;
+            } else if (error instanceof NotFoundException) {
+                throw error;
+            }
+            // Logs error to the  console
+            this.logger.error("Error finding store: ", error.message);
+            throw new InternalServerErrorException(
+                "An unexpected error occurred while finding  store"
+            );
+        }
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} store`;
+    }
+
+    update(id: number, updateStoreDto: UpdateStoreDto) {
+        return `This action updates a #${id} store`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} store`;
+    }
 }
