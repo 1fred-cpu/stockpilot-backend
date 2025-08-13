dd55d4b3ec1417198fd4b267fe4c625587840c6e
frederick <frederickfrimpong284@gmail.com>
2025-08-13 15:12:03 -0700

    update

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,90 +1,90 @@
 import {
-    IsString,
-    IsNotEmpty,
-    IsArray,
-    IsNumber,
-    IsObject,
-    IsUrl,
-    IsOptional
-} from "class-validator";
+  IsString,
+  IsNotEmpty,
+  IsArray,
+  IsNumber,
+  IsObject,
+  IsUrl,
+  IsOptional,
+} from 'class-validator';
 export class CreateProductDto {
-    @IsString()
-    @IsNotEmpty()
-    storeId: string;
+  @IsString()
+  @IsNotEmpty()
+  storeId: string;
 
-    @IsString()
-    @IsNotEmpty()
-    name: string;
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-    @IsString()
-    @IsNotEmpty()
-    brand: string;
+  @IsString()
+  @IsNotEmpty()
+  brand: string;
 
-    @IsString()
-    @IsNotEmpty()
-    category: string;
+  @IsString()
+  @IsNotEmpty()
+  category: string;
 
-    @IsString()
-    @IsNotEmpty()
-    description: string;
+  @IsString()
+  @IsNotEmpty()
+  description: string;
 
-    @IsUrl()
-    thumbnail: string;
+  @IsUrl()
+  thumbnail: string;
 
-    @IsArray()
-    @IsNotEmpty()
-    tags: string[];
+  @IsArray()
+  @IsNotEmpty()
+  tags: string[];
 
-    @IsOptional()
-    @IsObject()
-    attributes: Record<string, any>;
+  @IsOptional()
+  @IsObject()
+  attributes: Record<string, any>;
 
-    @IsArray()
-    variants: Variant[];
+  @IsArray()
+  variants: Variant[];
 }
 
 export class Variant {
-    @IsOptional()
-    @IsString()
-    @IsNotEmpty()
-    productId: string;
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  productId: string;
 
-    @IsUrl()
-    imageUrl: string;
+  @IsUrl()
+  imageUrl: string;
 
-    @IsString()
-    @IsNotEmpty()
-    color: string;
+  @IsString()
+  @IsNotEmpty()
+  color: string;
 
-    @IsString()
-    @IsNotEmpty()
-    size: string;
+  @IsString()
+  @IsNotEmpty()
+  size: string;
 
-    @IsString()
-    @IsNotEmpty()
-    weight: string;
+  @IsString()
+  @IsNotEmpty()
+  weight: string;
 
-    @IsString()
-    @IsNotEmpty()
-    dimensions: string;
+  @IsString()
+  @IsNotEmpty()
+  dimensions: string;
 
-    @IsNumber()
-    @IsNotEmpty()
-    stock: number;
+  @IsNumber()
+  @IsNotEmpty()
+  stock: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    price: number;
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    lowTStockThreshold: number;
+  @IsNumber()
+  @IsNotEmpty()
+  lowStockThreshold: number;
 
-    @IsNumber()
-    @IsNotEmpty()
-    reserved: number;
+  @IsNumber()
+  @IsNotEmpty()
+  reserved: number;
 
-    @IsString()
-    @IsNotEmpty()
-    sku: string;
+  @IsString()
+  @IsNotEmpty()
+  sku: string;
 }
