04f8d8f702d68d4ae2a09e2edab197532950ca24
frederick <frederickfrimpong284@gmail.com>
2025-08-29 23:29:05 -0700

    update files

--- utils/upload-file.ts
+++ utils/upload-file.ts
@@ -1,34 +1,29 @@
-import { Multer } from "multer";
-import { BadRequestException, Inject, Injectable } from "@nestjs/common";
+import { Multer } from 'multer';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 @Injectable()
 export class FileUploadService {
-    constructor(
-        @Inject("SUPABASE_CLIENT")
-        private readonly supabase: any
-    ) {}
+  constructor(
+    @Inject('SUPABASE_CLIENT')
+    private readonly supabase: any,
+  ) {}
 
-    async uploadFile(file: Multer.File | null, path: string, bucket: string) {
-        if (!file) return null;
+  async uploadFile(file: Multer.File | null, path: string, bucket: string) {
+    if (!file) return null;
 
+    const { data, error } = await this.supabase.storage
+      .from(bucket) // bucket name
+      .upload(path, file.buffer, {
+        cacheControl: '3600',
+        upsert: false,
+        contentType: file.mimetype,
+      });
 
-        const { data, error } = await this.supabase.storage
-            .from(bucket) // bucket name
-            .upload(path, file.buffer, {
-                cacheControl: "3600",
-                upsert: false,
-                contentType: file.mimetype
-            });
-
-        if (error) {
-            throw new BadRequestException(
-                `Error uploading file: ${error.message}`
-            );
-        }
-
-        // If bucket is PUBLIC:
-        const { data: pub } = this.supabase.storage
-            .from(bucket)
-            .getPublicUrl(path);
-        return pub.publicUrl;
+    if (error) {
+      throw new BadRequestException(`Error uploading file: ${error.message}`);
     }
+
+    // If bucket is PUBLIC:
+    const { data: pub } = this.supabase.storage.from(bucket).getPublicUrl(path);
+    return pub.publicUrl;
+  }
 }
