9ec0e416e7cd126339978e4edd7f3f670f8a5d6b
frederick <frederickfrimpong284@gmail.com>
2025-09-01 15:24:18 -0700

    update files

--- utils/get-path.ts
+++ utils/get-path.ts
@@ -1,8 +1,7 @@
-// const urlStr =
-//   'https://iqtoaznivtnfgsudkvzg.supabase.co/storage/v1/object/public/products/stores/Luxeline/1756495058282_classic-polo-shirt-navy-683.webp';
-// const path = urlStr.replace(/^https?:\/\/[^/]+/, '');
-// console.log(path); // => "/storage/v1/object/public/products/stores/Luxeline/1756495058282_classic-polo-shirt-navy-683.webp"
-
 export function getPathFromUrl(url: string): string {
-  return url.replace(/^https?:\/\/[^/]+/, '');
+  // Remove domain and the fixed prefix up to "products/"
+  return url.replace(
+    /^https?:\/\/[^/]+\/storage\/v1\/object\/public\/products\//,
+    '',
+  );
 }
